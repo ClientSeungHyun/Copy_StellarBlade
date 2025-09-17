0710d461c247110473144dce0193533002b4c164
양우송 <90258668+YangWooSong@users.noreply.github.com>
2025-09-18 00:10:12 +0900

    Change Player Model

---
+++ Source/Copy_StellarBlade/Animation/SB_Eve_AnimInstance.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animation/SB_Eve_AnimInstance.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+USB_Eve_AnimInstance::USB_Eve_AnimInstance()
+{
+}
+
+void USB_Eve_AnimInstance::NativeInitializeAnimation()
+{
+    Super::NativeInitializeAnimation();
+
+    Character = Cast<ACharacter>(GetOwningActor());
+
+    if (Character)
+    {
+        MovementComponent = Character->GetCharacterMovement();
+    }
+}
+
+void USB_Eve_AnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+    Super::NativeUpdateAnimation(DeltaSeconds);
+
+    if (Character == nullptr)
+    {
+        return;
+    }
+
+    if (MovementComponent == nullptr)
+    {
+        return;
+    }
+
+    Velocity = MovementComponent->Velocity;
+    GroundSpeed = Velocity.Size2D();
+
+    bShouldMove = GroundSpeed > 3.f && MovementComponent->GetCurrentAcceleration() != FVector::ZeroVector;
+
+    bIsFalling = MovementComponent->IsFalling();
+}
